0464445b33bc4d074769cb786136599d21247a1a
Zabrina L <zabrinatan@hotmail.com>
2019-03-30 00:37:20 +1100

    import emotions in routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
-
 import Home from './components/Home';
+import Emotions from './components/Home';motions
 
 const Routes = (
   <Router>
