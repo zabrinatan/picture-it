dd49b9b34427ae1a698846b24723d7f34acb6f4a
Zabrina L <zabrinatan@hotmail.com>
2019-03-30 00:37:55 +1100

    fix routes emotion typo

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Emotions from './components/Home';motions
+import { HashRouter as Router, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Emotions from './components/Emotions'
 
 const Routes = (
   <Router>
