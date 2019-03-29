47ed3ef776331a2f7a91c3e00667eca5cfb6d105
Zabrina L <zabrinatan@hotmail.com>
2019-03-30 00:30:41 +1100

    integrate router and create home component

---
+++ src/Routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
+import Home from './components/Home';
+
+const Routes = (
+  <Router>
+    <div>
+      <Route exact path = "/" component = {Home} />
+    </div>
+  </Router>
+)
+
+export default Routes;
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+
+  render() {
+    return(
+      <div>
+        <div id="home-container">
+          <h1 id="title">Picture it</h1>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Home;
